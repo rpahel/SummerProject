9a3b8dd39dfb95d607faf9a93cf2bc189ceeee90
rpahel <92730433+rpahel@users.noreply.github.com>
2025-07-25 17:16:03 +0200

    Initial commit

---
+++ Source/SummerProject/SummerProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SummerProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SummerProject, "SummerProject" );
