2fda2dd2b0c7a5af2f45cb5c7139b8d7436b3685
Cocomineral <78883073+corentinch59@users.noreply.github.com>
2025-08-07 14:21:19 +0200

    Fix for uninteractable objective

--- Source/SummerProject/IA/RoutineComponent.cpp
+++ Source/SummerProject/IA/RoutineComponent.cpp
@@ -16,6 +16,7 @@ void URoutineComponent::InteractWithObjective()
 	UInteractableComponent* interactable = GetCurrentObjectiveInteractable();
 	if (!interactable)
 	{
+		GoNextObjective();
 		return;
 	}
 
